215d329063a144b45eed763c6ec91ca050494e0a
Mike Hartley <mhartley19@gmail.com>
2021-05-18 13:51:58 -0500

    first commit

---
+++ src/components/basic.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function GameFunction(){
+    const [playerCardCount, setPlayerCardCount] = useState('')
+    const [dealerCardCount, setDealerCardCount] = useState('')
+
+
+    const cardRandomizer = () => {
+
+    }
+
+}
